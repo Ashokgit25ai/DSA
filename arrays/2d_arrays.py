240d0ffc18d6775fdae673212603e884f22f8d6d
ashok <anithaashok944@gmail.com>
2026-05-07 16:25:00 +0530

    error clear

--- arrays/2d_arrays.py
+++ arrays/2d_arrays.py
@@ -2,20 +2,22 @@
 # Output: true
 
 def find_num(arr,target):
+    rows = len(arr)
+    cols = len(arr[0])
     left = 0
-    right = ((len(arr) * len(arr[0]))-1)
+    right = ((rows * cols)-1)
     mid = (left + right) // 2
     
     while left <= right:
-        row = mid // right
-        col = mid % right
+        row = mid // cols
+        col = mid % cols
         mid_val = arr[row][col]
         if mid_val == target:
             return True
         elif mid_val < target:
-            right = mid-1
+            left = mid+1
         else:
-            left = mid+1        
+            right = mid-1        
     return False
 
 arr = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
