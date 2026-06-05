82350afe6e269df401d0c84276b8d0be9dc254c5
Ashokgit25ai <anithaashok944@gmail.com>
2026-06-05 18:22:20 +0530

    LC 560 Subarray Sum Equals K

--- Leetcode_Problems/Arrays/subarray_sum_equals_k.py
+++ Leetcode_Problems/Arrays/subarray_sum_equals_k.py
@@ -9,20 +9,42 @@
 # Output: 2
 
 def subarraySum(nums, k):
+        # Stores the final answer
         count = 0
-        prefix_sum = 0
-        store_value = {0:1}
 
+        # Running prefix sum
+        prefix_sum = 0
+
+        # HashMap: {prefix_sum : frequency}
+        # We start with {0:1} because before processing any element
+        # the prefix sum is 0 and it has occurred once.
+        prefix_map = {0: 1}
+
+        # Traverse the array
         for num in nums:
+
+            # Update running prefix sum
             prefix_sum += num
 
-            if prefix_sum - k in store_value:
-                count += store_value[prefix_sum - k]
-            
-            if prefix_sum in store_value:
-                store_value[prefix_sum] += 1
+            # We need a previous prefix sum such that:
+            # current_prefix - previous_prefix = k
+            #
+            # Therefore:
+            # previous_prefix = current_prefix - k
+            needed = prefix_sum - k
+
+            # If we've seen 'needed' before,
+            # then we found one or more valid subarrays
+            if needed in prefix_map:
+
+                # Add the number of times that prefix sum appeared
+                count += prefix_map[needed]
+
+            # Store/update the current prefix sum frequency
+            if prefix_sum in prefix_map:
+                prefix_map[prefix_sum] += 1
             else:
-                store_value[prefix_sum] = 1
+                prefix_map[prefix_sum] = 1
 
         return count
 nums = [1,1,1]
